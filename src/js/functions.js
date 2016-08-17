ee8e197140220e86114becff49ea942481b6437d
ira <bakum21@ukr.net>
2016-08-17 12:29:42 +0300

    changed list product, cart, account

--- src/js/functions.js
+++ src/js/functions.js
@@ -313,4 +313,14 @@ jQuery(document).ready(function() {
         $(this).parent().find('.short').removeClass('short');
         $(this).remove();
     });
+
+
+
+    // show or hide category in list products
+    $(".list_product .list_category .show_all").on('click', function () {
+        $(this).closest(".list_category").addClass("open");
+    });
+    $(".list_product .list_category .hide_item").on('click', function () {
+        $(this).closest(".list_category").removeClass("open");
+    });
 });
